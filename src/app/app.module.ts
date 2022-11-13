0b4a11c32db1be9c6eac8285b6634c68dd3ee5ac
anantraghuvanshi <anantsinghraghuvanshi1@gmail.com>
2022-11-13 22:57:36 +0530

    server add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
