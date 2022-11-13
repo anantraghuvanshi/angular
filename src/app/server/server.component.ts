debe856d852b9b20d9a157e69e1073ccc55f24f5
anantraghuvanshi <anantsinghraghuvanshi1@gmail.com>
2022-11-13 22:52:16 +0530

    server create

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-server',
-    templateUrl: 
+    templateUrl: './server.component.html'
 })
 
 export class ServerComponent{
