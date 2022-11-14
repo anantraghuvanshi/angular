f7b73cc0c78fc3b5936265de0944347bde6c4878
anantraghuvanshi <anantsinghraghuvanshi1@gmail.com>
2022-11-14 16:27:43 +0530

    section 2 complete

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName ='TestServer';
   serverCreated = false;
+  servers = ['Testserver', 'TestServer 2'];
 
 
   constructor(){
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
   }
   onCreateServer(){
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is' + this.serverName ;
   }
   onUpdateServerName(event:Event){
